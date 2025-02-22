44695fee91e85469285455568cc07cfb2d416d3f
budyn <115996840+Budyn8@users.noreply.github.com>
2025-02-22 17:39:13 +0100

    Bot is working git add .!

--- bot.py
+++ bot.py
@@ -1,68 +1,79 @@
 from datetime import date
-from discord import Client, Intents, Message, app_commands, Interaction
+from os import getenv
+from typing_extensions import override
+from discord import Intents, Interaction, Message, app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
+
+ALLOWED_TO_USE_SYNC = []
+GROUPS = ["T1", "T2", "T3"]
+SUBJECTS = ["ANL2", "PRM2", "SYCY"]
+GUILDS = []
 
 if not load_dotenv():
     print("No new Env Variables")
 
-
-# Podejrzewam że wolisz jak stworzymy classe oddzielną do tego :.(
-class Event:
-    e_date: date
-    e_group: str
-    e_subject: str
-
-    def __init__(self):
-        self.e_date = date(1, 1, 1)
-        self.e_group = "def"
-        self.e_subject = "def"
-
-    @staticmethod
-    def from_str(smth: list[str]):
-        # TODO Dodać sprawdzanie czy ilość argumentów się zgadza, czy argumenty spełniają requirementy
-        e_date = date(int(smth[0]), int(smth[1]), int(smth[2]))
-        e_group = smth[3]
-        e_subject = smth[4]
-        return [e_date, e_group, e_subject]
+TOKEN = getenv("DISCORD_TOKEN")
+if TOKEN == None:
+    exit(-1)
 
 
-class BotPlayer(Client):
-    async def on_ready(self):
-        print(f"READY PLAYER {self.user}")
-
-    async def on_message(self, message: Message):
-        print(f"Message from {message.author}: {message.content}")
-        if "blob" in message.content:
-            _ = await message.channel.send("bolb")
+def is_owner(ctx: commands.Context[commands.Bot]):
+    return ctx.author.id in ALLOWED_TO_USE_SYNC
 
 
 intents = Intents.default()
 intents.message_content = True
+intents.guilds = True
+intents.messages = True
+intents.presences = True
 
-client = BotPlayer(intents=intents)
-
-bot = commands.Bot(command_prefix="/", intents=intents)
-
-
-@bot.command(
-    description="Adds described event to calendar",
-    help="You need to specify: Date(dd:mm:yy), Group, Subject, type of event",
-)
-async def add_event(ctx, *args):
-    """Adds described event to calendar using sqlite db"""
-    pass
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
-# tree = app_commands(client)
-# client.tree = tree
+@bot.event
+async def on_ready():
+    print(f"Player that is ready: {bot.user}")
 
 
-# @client.tree.command("add_event", description="Adds described event to calendar")
+@bot.listen("on_message")
+async def on_message(message: Message):
+    print(f"Message from {message.author} : {message.content}")
+    if message.author != bot.user and "blob" in message.content:
+        _ = await message.channel.send("blob")
+
+
+@bot.command(name="sync", description="Owners Only, sync commands")
+@commands.check(is_owner)
+async def sync(ctx: commands.Context[commands.Bot]):
+    print(f"Sync commands {bot.tree.get_commands().__len__()}")
+
+    if ctx.guild == None:
+        print("Couldn't get guild to sync with")
+        return
+
+    cmdsynced = await bot.tree.sync(guild=ctx.guild)
+
+    _ = await ctx.send(f"Commands Synced: {len(cmdsynced)}")
+
+
+@bot.tree.command()
+@app_commands.guilds(*GUILDS)
 @app_commands.describe(
-    message="You need to specify: Date(dd:mm:yy), Group, Subject, type of event"
+    date="A date in format of dd(sep)mm(sep)yy where (sep) = - or / or :"
 )
-async def add_event_to_calendar(
-    interaction: Interaction, date: str, group: int, subject: str, type_of_event: str
-) -> None:
-    _ = await interaction.response.send_message((date, group, subject, type_of_event))
+@app_commands.choices(
+    group=[app_commands.Choice(name=group, value=group) for group in GROUPS],
+    subject=[app_commands.Choice(name=subject, value=subject) for subject in SUBJECTS],
+)
+async def add_event(
+    interaction: Interaction, date: str, group: str, subject: str, event: str
+):
+    """Adds described event to calendar using sqlite db"""
+    print("Adding event: ")
+    _ = await interaction.response.send_message(
+        f"There will be {event} for {group} in {subject} at {date}"
+    )
+
+
+bot.run(TOKEN)
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from bot import client
-from calendar.add_event import add_calendar_event
+from bot import bot
 from os import getenv
 from dotenv import load_dotenv
 
 if not load_dotenv():
-    print("Not new Variables.")
+    print("No new Variables.")
 
 
 def main():
     bot_token = getenv("DISCORD_TOKEN")
     if bot_token == None:
         exit(-1)
-    client.run(bot_token)
+    bot.run(bot_token)
+
+
+main()
