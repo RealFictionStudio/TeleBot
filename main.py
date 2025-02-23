ed076fbd8345821b522606b55cc93ef0f2e25d53
realfictionstudio <realfictionstudio@gmail.com>
2025-02-23 23:45:02 +0100

    intro to handling plugins

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from bot import client
-from os import getenv
+from os import getenv, getcwd, listdir, path
 from dotenv import load_dotenv
 load_dotenv()
+
+
+def load_plugins(plugin_path: str = getcwd()):
+    for file in list(listdir(plugin_path)):
+        if path.isdir(file) and file[0] not in "_.":
+            try:
+                print(f"Trying to load plugin: {file}")
+            except Exception as e:
+                print(e)
 
 
 def main():
